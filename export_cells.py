d6de0fb0bb1c5f82e5042c577200eb580a15284f
Jonathan Poulter <poulter7@gmail.com>
2017-08-31 21:10:51 -0400

    GraphicX

--- export_cells.py
+++ export_cells.py
@@ -4,11 +4,14 @@ from optparse import OptionParser
 import logging
 import os
 import json
+import base64
 
 
 def parse_ipynb(ipynb_json):
     source = collections.defaultdict(list)
     output = collections.defaultdict(list)
+    images = collections.defaultdict(list)
+    latex = collections.defaultdict(list)
 
     for cell in ipynb_json['cells']:
         for tag in cell['metadata'].get('tags', []):
@@ -22,19 +25,25 @@ def parse_ipynb(ipynb_json):
                     output_type = cell_output.get('output_type')
                     if output_type == 'stream':
                         output[tag].extend(map(unicode.rstrip, cell_output['text']))
+                    elif output_type == 'display_data':
+                        string_rep = cell_output['data']['image/png']
+                        image = base64.b64decode(string_rep)
+                        images[tag].append(image)
                     elif output_type == 'execute_result':
                         for datatype, value in cell_output['data'].items():
                             if datatype == 'text/plain':
                                 output[tag].extend(map(unicode.rstrip, value))
+                            elif datatype == 'text/latex':
+                                latex[tag].extend(map(unicode.rstrip, value))
                             else:
                                 logging.warn("Unable to process datatype {0}".format(datatype))
                     else:
 
                         logging.warn("Unable to process output_type {0}".format(output_type))
-    return source, output
+    return source, output, images, latex
 
 
-def save_ipynb_cells(cell_sources, cell_outputs, output_dir):
+def save_ipynb_cells(cell_sources, cell_outputs, cell_images, cell_latex, output_dir):
     try:
         os.makedirs(os.path.join(output_dir))
     except os.error as e:
@@ -46,7 +55,20 @@ def save_ipynb_cells(cell_sources, cell_outputs, output_dir):
             path = os.path.join(output_dir, '{0}.{1}'.format(tag, category))
             logging.info("Exporting to {0}".format(path))
             with open(path, 'w') as f:
-                f.write("\n".join(value))
+                try:
+                    f.write("\n".join(value))
+                except:
+                    pass
+    for tag, images in cell_images.items():
+        for image in images:
+            path = os.path.join(output_dir, '{0}.{1}'.format(tag, 'png'))
+            with open(path, 'wb') as f:
+                f.write(image)
+    for tag, latexs in cell_latex.items():
+        for latex in latexs:
+            path = os.path.join(output_dir, '{0}.{1}'.format(tag, 'tex'))
+            with open(path, 'w') as f:
+                f.write(latex)
 
 
 def load_ipynb(path):
@@ -55,7 +77,7 @@ def load_ipynb(path):
 
 def extract_cells(ipynb_path, base_dir=None):
     ipynb_path = os.path.abspath(ipynb_path)
-    sources, outputs = parse_ipynb(load_ipynb(ipynb_path))
+    sources, outputs, images, latex = parse_ipynb(load_ipynb(ipynb_path))
 
     ipynb_dirname = os.path.dirname(ipynb_path)
     ipynb_filename = os.path.basename(ipynb_path)
@@ -66,4 +88,4 @@ def extract_cells(ipynb_path, base_dir=None):
         output_dir = os.path.normcase(os.path.join(base_dir, os.path.relpath(ipynb_path.replace('.ipynb', ''))))
 
     output_dir = os.path.join(output_dir, '.cells', ipynb_filename.replace('.ipynb', ''))
-    save_ipynb_cells(sources, outputs, output_dir)
+    save_ipynb_cells(sources, outputs, images, latex, output_dir)
--- test.py
+++ test.py
@@ -7,13 +7,14 @@ import os
 def test_load_ipynb_correctly():
     """Check the parsing works"""
     nb = export_cells.load_ipynb('notebook.ipynb')
-    parsed_source, parsed_output = export_cells.parse_ipynb(nb)
+    parsed_source, parsed_output, parsed_images, parsed_latex = export_cells.parse_ipynb(nb)
 
     assert parsed_source['tag1'][0] == u'for i in range(3):'
     assert parsed_source['tag1'][1] == u'    print i'
     assert parsed_source['tag1'][2] == u'for i in range(3, 6):'
     assert parsed_source['tag1'][3] == u'    print i'
     assert parsed_source['tag2'][0] == u"print 'a'"
+    assert len(parsed_images) == 1
     logging.fatal(parsed_source['tag3'])
 
 
@@ -33,5 +34,7 @@ def test_files_exported():
     assert os.path.exists(os.path.join(tempdir, 'notebook', '.cells', 'notebook', 'tag1.output'))
     assert os.path.exists(os.path.join(tempdir, 'notebook', '.cells', 'notebook', 'tag2.source'))
     assert os.path.exists(os.path.join(tempdir, 'notebook', '.cells', 'notebook', 'tag2.output'))
+    assert os.path.exists(os.path.join(tempdir, 'notebook', '.cells', 'notebook', 'img.png'))
+    assert os.path.exists(os.path.join(tempdir, 'notebook', '.cells', 'notebook', 'latex.tex'))
     assert os.path.exists(os.path.join(tempdir, 'src', 'notebook', '.cells', 'notebook', 'tag1.source'))
 
